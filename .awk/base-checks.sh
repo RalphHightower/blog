02af6c99d58f121db4c2389bbc3e45b2d1ad6fd5
Ralph Hightower <32745442+RalphHightower@users.noreply.github.com>
2025-10-08 01:02:36 -0400

    [cleanup](security): Action(can't be pinned)
    Signed-off-by: Ralph Hightower <32745442+RalphHightower@users.noreply.github.com>

--- .awk/base-checks.sh
+++
@@ -1,6 +0,0 @@
-npm ci
-npm install markdownlint-cli2 --global
-npx semantic-release
-npm install @semantic-release/release-notes-generator -D
-npm install @semantic-release/commit-analyzer -D
-npm install @semantic-release/changelog -D
